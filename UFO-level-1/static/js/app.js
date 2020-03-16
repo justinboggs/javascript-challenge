6c0fabcde51ba25ce0ce26f2e6c6eb102b898ae4
Justin Boggs <jboggs1@gmail.com>
2020-03-16 16:07:23 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(tableData);
+
+// Step 1: Loop Through `data` and console.log each ufo report object
+tableData.forEach(function (ufoReport) {
+    console.log(ufoReport);
+});
+
