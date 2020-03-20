d2495f1d0f6c2b52615020038a52180da74aa635
Justin Boggs <jboggs1@gmail.com>
2020-03-20 09:49:16 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,59 +4,38 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-// console.log(tableData);
-
+// build the function to loop through and append the data to a table
 function tableBuild(data) {
     tbody.html("");
-    
     // Step 1: Loop Through `data` and console.log each ufo report object
-    tableData.forEach(function(data) {
+    data.forEach((dataRow) => {
         // console.log(data);
-    });
-
-    // Step 2:  Use d3 to append one table row `tr` for each ufo report object
-    tableData.forEach(function(data) {
-        // console.log(data);
-        var row = tbody.append("tr");
-    });
-
-    // Step 3:  Use `Object.entries` to console.log each ufo report value
-    tableData.forEach(function(data) {
-        // console.log(data);
-        var row = tbody.append("tr");
-
-        Object.entries(data).forEach(function([key, value]) {
-            // console.log(key, value);
-        });
-    });
-
-    // Step 5: Use d3 to update each cell's text with
-    // ufo report values (weekday, date, high, low)
-    tableData.forEach(function(data) {
-        // console.log(data);
-        var row = tbody.append("tr");
-        Object.entries(data).forEach(function([key, value]) {
+        // Step 2:  Use d3 to append one table row `tr` for each ufo report object
+        const row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
             // console.log(key, value);
             // Append a cell to the row for each value
             // in the ufo report object
-            var cell = row.append("td");
-            cell.html(value);
+            let cell = row.append("td");
+            cell.text(val);
         });
     });
 
 };
 
 function dtfilter() {
-    const filterDate = d3.select("#datetime").property("value");
+    const justin = d3.select("#datetime").property("value");
     // 
     let filteredData = tableData; 
-    if (filterDate) {
-        filteredData = filteredData.filter(row => row.datetime === filterDate)
+    if (justin) {
+        filteredData = filteredData.filter(row => row.datetime === justin)
     };
+    tableBuild(filteredData);
 };
 
+// Attach to the 
 d3.selectAll("#filter-btn").on("click", dtfilter);
 
-
+// Build the table on page load
+tableBuild(tableData);
 
